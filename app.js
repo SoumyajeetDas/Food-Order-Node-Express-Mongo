383fb2b6ca4cd750c6ce9ce44d40a8b678e8e07a
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-11-01 19:39:40 +0530

    Adding Front End domain in CORS Policy

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000",'https://bengali-food-webapp.herokuapp.com'],
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
