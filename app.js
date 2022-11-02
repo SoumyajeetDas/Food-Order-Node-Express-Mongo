c3495d7233b86e6a79abcbcf25ad35c612143bdd
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-11-02 15:47:12 +0530

    Removed Heroku App Domai from CORS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000",'https://bengali-food-webapp.herokuapp.com', 'https://bengali-food-webapp.netlify.app'],
+    origin: ["http://localhost:3000", 'https://bengali-food-webapp.netlify.app'],
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
